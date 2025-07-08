2ef4c553acb8706f9ef4e221d0d5d9a313412a20
sentares <aydararzybekov2002@mail.ru>
2025-07-08 19:02:55 +0600

    fix: update the ContactListPage and FilterForm with adding state logic

--- src/apps/MainApp/MainApp.tsx
+++ src/apps/MainApp/MainApp.tsx
@@ -1,81 +1,112 @@
-import React, {useState} from 'react';
-import './MainApp.scss';
-import {ThemeProvider} from 'react-bootstrap';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import {Layout} from 'src/components/Layout';
-import {ContactListPage, GroupPage, ContactPage, FavoritListPage, GroupListPage} from 'src/pages';
-import {ContactDto} from 'src/types/dto/ContactDto';
-import {FavoriteContactsDto} from 'src/types/dto/FavoriteContactsDto';
-import {GroupContactsDto} from 'src/types/dto/GroupContactsDto';
-import {DATA_CONTACT, DATA_GROUP_CONTACT} from 'src/__data__';
+import { useState } from 'react'
+import { ThemeProvider } from 'react-bootstrap'
+import { Provider } from 'react-redux'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { PersistGate } from 'redux-persist/integration/react'
+import { DATA_CONTACT, DATA_GROUP_CONTACT } from 'src/__data__'
+import { Layout } from 'src/components/Layout'
+import {
+	ContactListPage,
+	ContactPage,
+	FavoritListPage,
+	GroupListPage,
+	GroupPage,
+} from 'src/pages'
+import { persistor, store } from 'src/redux/store'
+import { ContactDto } from 'src/types/dto/ContactDto'
+import { FavoriteContactsDto } from 'src/types/dto/FavoriteContactsDto'
+import { GroupContactsDto } from 'src/types/dto/GroupContactsDto'
+import './MainApp.scss'
 
 export const MainApp = () => {
-  const contactsState = useState<ContactDto[]>(DATA_CONTACT);
-  const favoriteContactsState = useState<FavoriteContactsDto>([
-    DATA_CONTACT[0].id,
-    DATA_CONTACT[1].id,
-    DATA_CONTACT[2].id,
-    DATA_CONTACT[3].id
-  ]);
-  const groupContactsState = useState<GroupContactsDto[]>(DATA_GROUP_CONTACT);
+	const contactsState = useState<ContactDto[]>(DATA_CONTACT)
+	const favoriteContactsState = useState<FavoriteContactsDto>([
+		DATA_CONTACT[0].id,
+		DATA_CONTACT[1].id,
+		DATA_CONTACT[2].id,
+		DATA_CONTACT[3].id,
+	])
+	const groupContactsState = useState<GroupContactsDto[]>(DATA_GROUP_CONTACT)
 
-  return (
-    <ThemeProvider
-      breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}
-      minBreakpoint="xxs"
-    >
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={
-              <ContactListPage
-                contactsState={contactsState}
-                favoriteContactsState={favoriteContactsState}
-                groupContactsState={groupContactsState}
-              />
-            } />
-            <Route path="contact">
-              <Route index element={
-                <ContactListPage
-                  contactsState={contactsState}
-                  favoriteContactsState={favoriteContactsState}
-                  groupContactsState={groupContactsState}
-                />
-              } />
-              <Route path=":contactId" element={
-                <ContactPage
-                  contactsState={contactsState}
-                  favoriteContactsState={favoriteContactsState}
-                  groupContactsState={groupContactsState}
-                />
-              } />
-            </Route>
-            <Route path="groups">
-              <Route index element={
-                <GroupListPage
-                  contactsState={contactsState}
-                  favoriteContactsState={favoriteContactsState}
-                  groupContactsState={groupContactsState}
-                />
-              } />
-              <Route path=":groupId" element={
-                <GroupPage
-                  contactsState={contactsState}
-                  favoriteContactsState={favoriteContactsState}
-                  groupContactsState={groupContactsState}
-                />
-              } />
-            </Route>
-            <Route path="favorit" element={
-              <FavoritListPage
-                contactsState={contactsState}
-                favoriteContactsState={favoriteContactsState}
-                groupContactsState={groupContactsState}
-              />
-            } />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
-  );
-};
+	return (
+		<Provider store={store}>
+			<PersistGate persistor={persistor} loading={<div>Loading...</div>}>
+				<ThemeProvider
+					breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}
+					minBreakpoint='xxs'
+				>
+					<BrowserRouter>
+						<Routes>
+							<Route path='/' element={<Layout />}>
+								<Route
+									index
+									element={
+										<ContactListPage
+										// contactsState={contactsState}
+										// favoriteContactsState={favoriteContactsState}
+										// groupContactsState={groupContactsState}
+										/>
+									}
+								/>
+								<Route path='contact'>
+									<Route
+										index
+										element={
+											<ContactListPage
+											// contactsState={contactsState}
+											// favoriteContactsState={favoriteContactsState}
+											// groupContactsState={groupContactsState}
+											/>
+										}
+									/>
+									<Route
+										path=':contactId'
+										element={
+											<ContactPage
+												contactsState={contactsState}
+												favoriteContactsState={favoriteContactsState}
+												groupContactsState={groupContactsState}
+											/>
+										}
+									/>
+								</Route>
+								<Route path='groups'>
+									<Route
+										index
+										element={
+											<GroupListPage
+												contactsState={contactsState}
+												favoriteContactsState={favoriteContactsState}
+												groupContactsState={groupContactsState}
+											/>
+										}
+									/>
+									<Route
+										path=':groupId'
+										element={
+											<GroupPage
+												contactsState={contactsState}
+												favoriteContactsState={favoriteContactsState}
+												groupContactsState={groupContactsState}
+											/>
+										}
+									/>
+								</Route>
+								<Route
+									path='favorit'
+									element={
+										<FavoritListPage
+											contactsState={contactsState}
+											favoriteContactsState={favoriteContactsState}
+											groupContactsState={groupContactsState}
+										/>
+									}
+								/>
+							</Route>
+						</Routes>
+					</BrowserRouter>
+				</ThemeProvider>
+			</PersistGate>
+		</Provider>
+	)
+}
--- src/components/FilterForm.tsx
+++ src/components/FilterForm.tsx
@@ -1,58 +1,68 @@
-import {Formik} from 'formik';
-import {Button, Col, Form, InputGroup, Row} from 'react-bootstrap';
-import React, {memo} from 'react';
-import {FormikConfig} from 'formik/dist/types';
-import {GroupContactsDto} from 'src/types/dto/GroupContactsDto';
+import { Formik } from 'formik'
+import { FormikConfig } from 'formik/dist/types'
+import { memo, useEffect } from 'react'
+import { Button, Col, Form, InputGroup, Row } from 'react-bootstrap'
+import { fetchGroups } from 'src/redux/groups'
+import { useAppDispatch, useAppSelector } from 'src/redux/hooks'
 
 export interface FilterFormValues {
-  name: string,
-  groupId: string
+	name: string
+	groupId: string
 }
 
-interface FilterFormProps extends FormikConfig<Partial<FilterFormValues>> {
-  groupContactsList: GroupContactsDto[]
+interface FilterFormProps {
+	onSubmit: (values: FilterFormValues) => void
 }
 
-export const FilterForm = memo<FilterFormProps>(({
-  onSubmit,
-  initialValues = {},
-  groupContactsList
-}) => {
-  return (
-    <Formik initialValues={initialValues} onSubmit={onSubmit}>
-      {({handleChange, handleSubmit}) => (
-        <Form onSubmit={handleSubmit} onChange={handleSubmit}>
-          <Row xxl={4} className="g-4">
-            <Col>
-              <InputGroup className="mb-3">
-                <Form.Control
-                  id={'name'}
-                  name={'name'}
-                  onChange={handleChange}
-                  placeholder="name"
-                  aria-label="name"
-                />
-              </InputGroup>
-            </Col>
-            <Col>
-              <Form.Select
-                id={'groupId'}
-                name={'groupId'}
-                aria-label="Поиск по группе"
-                onChange={handleChange}
-              >
-                <option>Open this select menu</option>
-                {groupContactsList.map((groupContacts) => (
-                  <option value={groupContacts.id} key={groupContacts.id}>{groupContacts.name}</option>
-                ))}
-              </Form.Select>
-            </Col>
-            <Col>
-              <Button variant={'primary'} type={'submit'}>Применить</Button>
-            </Col>
-          </Row>
-        </Form>
-      )}
-    </Formik>
-  );
+export const FilterForm = memo<FilterFormProps>(({ onSubmit }) => {
+	const dispatch = useAppDispatch()
+
+	const { data } = useAppSelector(state => state.groups)
+	const { filter } = useAppSelector(state => state.contacts)
+
+	useEffect(() => {
+		dispatch(fetchGroups())
+	}, [])
+
+	return (
+		<Formik initialValues={filter} onSubmit={onSubmit}>
+			{({ handleChange, handleSubmit }) => (
+				<Form onSubmit={handleSubmit}>
+					<Row xxl={4} className='g-4'>
+						<Col>
+							<InputGroup className='mb-3'>
+								<Form.Control
+									id={'name'}
+									name={'name'}
+									onChange={handleChange}
+									placeholder='name'
+									aria-label='name'
+								/>
+							</InputGroup>
+						</Col>
+						<Col>
+							<Form.Select
+								id={'groupId'}
+								name={'groupId'}
+								aria-label='Поиск по группе'
+								onChange={handleChange}
+							>
+								<option value=''>Open this select menu</option>
+								{data.map(groupContacts => (
+									<option value={groupContacts.id} key={groupContacts.id}>
+										{groupContacts.name}
+									</option>
+								))}
+							</Form.Select>
+						</Col>
+						<Col>
+							<Button variant={'primary'} type='submit'>
+								Применить
+							</Button>
+						</Col>
+					</Row>
+				</Form>
+			)}
+		</Formik>
+	)
 })

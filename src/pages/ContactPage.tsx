28032eb8b1db8c22beb668d255aece9738ae69fc
sentares <aydararzybekov2002@mail.ru>
2025-07-09 18:46:30 +0600

    fix: update Contact Page with using redux

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,27 +1,24 @@
-import React, {FC, useEffect, useState} from 'react';
-import {CommonPageProps} from './types';
-import {Col, Row} from 'react-bootstrap';
-import {useParams} from 'react-router-dom';
-import {ContactDto} from 'src/types/dto/ContactDto';
-import {ContactCard} from 'src/components/ContactCard';
-import {Empty} from 'src/components/Empty';
+import { FC, useMemo } from 'react'
+import { Col, Row } from 'react-bootstrap'
+import { useParams } from 'react-router-dom'
+import { ContactCard } from 'src/components/ContactCard'
+import { Empty } from 'src/components/Empty'
+import { useAppSelector } from 'src/redux/hooks'
 
+export const ContactPage: FC = () => {
+	const { contactId } = useParams<{ contactId: string }>()
+	const { contactsList } = useAppSelector(state => state.contacts)
 
-export const ContactPage: FC<CommonPageProps> = ({
-  contactsState
-}) => {
-  const {contactId} = useParams<{ contactId: string }>();
-  const [contact, setContact] = useState<ContactDto>();
+	const contact = useMemo(
+		() => contactsList.find(({ id }) => id === contactId),
+		[contactId, contactsList]
+	)
 
-  useEffect(() => {
-    setContact(() => contactsState[0].find(({id}) => id === contactId));
-  }, [contactId]);
-
-  return (
-    <Row xxl={3}>
-      <Col className={'mx-auto'}>
-        {contact ? <ContactCard contact={contact} /> : <Empty />}
-      </Col>
-    </Row>
-  );
-};
+	return (
+		<Row xxl={3}>
+			<Col className={'mx-auto'}>
+				{contact ? <ContactCard contact={contact} /> : <Empty />}
+			</Col>
+		</Row>
+	)
+}

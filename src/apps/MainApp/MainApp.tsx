aa93ad8b2c12fa5d5bbd33b99f3a0e2f677a8d63
sentares <aydararzybekov2002@mail.ru>
2025-07-09 18:45:11 +0600

    feat: using redux in pages

--- src/apps/MainApp/MainApp.tsx
+++ src/apps/MainApp/MainApp.tsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
 import { ThemeProvider } from 'react-bootstrap'
 import { Provider } from 'react-redux'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { PersistGate } from 'redux-persist/integration/react'
-import { DATA_CONTACT, DATA_GROUP_CONTACT } from 'src/__data__'
 import { Layout } from 'src/components/Layout'
 import {
 	ContactListPage,
@@ -13,21 +11,9 @@ import {
 	GroupPage,
 } from 'src/pages'
 import { persistor, store } from 'src/redux/store'
-import { ContactDto } from 'src/types/dto/ContactDto'
-import { FavoriteContactsDto } from 'src/types/dto/FavoriteContactsDto'
-import { GroupContactsDto } from 'src/types/dto/GroupContactsDto'
 import './MainApp.scss'
 
 export const MainApp = () => {
-	const contactsState = useState<ContactDto[]>(DATA_CONTACT)
-	const favoriteContactsState = useState<FavoriteContactsDto>([
-		DATA_CONTACT[0].id,
-		DATA_CONTACT[1].id,
-		DATA_CONTACT[2].id,
-		DATA_CONTACT[3].id,
-	])
-	const groupContactsState = useState<GroupContactsDto[]>(DATA_GROUP_CONTACT)
-
 	return (
 		<Provider store={store}>
 			<PersistGate persistor={persistor} loading={<div>Loading...</div>}>
@@ -38,70 +24,16 @@ export const MainApp = () => {
 					<BrowserRouter>
 						<Routes>
 							<Route path='/' element={<Layout />}>
-								<Route
-									index
-									element={
-										<ContactListPage
-										// contactsState={contactsState}
-										// favoriteContactsState={favoriteContactsState}
-										// groupContactsState={groupContactsState}
-										/>
-									}
-								/>
+								<Route index element={<ContactListPage />} />
 								<Route path='contact'>
-									<Route
-										index
-										element={
-											<ContactListPage
-											// contactsState={contactsState}
-											// favoriteContactsState={favoriteContactsState}
-											// groupContactsState={groupContactsState}
-											/>
-										}
-									/>
-									<Route
-										path=':contactId'
-										element={
-											<ContactPage
-												contactsState={contactsState}
-												favoriteContactsState={favoriteContactsState}
-												groupContactsState={groupContactsState}
-											/>
-										}
-									/>
+									<Route index element={<ContactListPage />} />
+									<Route path=':contactId' element={<ContactPage />} />
 								</Route>
 								<Route path='groups'>
-									<Route
-										index
-										element={
-											<GroupListPage
-												contactsState={contactsState}
-												favoriteContactsState={favoriteContactsState}
-												groupContactsState={groupContactsState}
-											/>
-										}
-									/>
-									<Route
-										path=':groupId'
-										element={
-											<GroupPage
-												contactsState={contactsState}
-												favoriteContactsState={favoriteContactsState}
-												groupContactsState={groupContactsState}
-											/>
-										}
-									/>
+									<Route index element={<GroupListPage />} />
+									<Route path=':groupId' element={<GroupPage />} />
 								</Route>
-								<Route
-									path='favorit'
-									element={
-										<FavoritListPage
-											contactsState={contactsState}
-											favoriteContactsState={favoriteContactsState}
-											groupContactsState={groupContactsState}
-										/>
-									}
-								/>
+								<Route path='favorit' element={<FavoritListPage />} />
 							</Route>
 						</Routes>
 					</BrowserRouter>
--- src/pages/FavoritListPage.tsx
+++ src/pages/FavoritListPage.tsx
@@ -1,24 +1,28 @@
-import React, {memo, useEffect, useState} from 'react';
-import {CommonPageProps} from './types';
-import {Col, Row} from 'react-bootstrap';
-import {ContactCard} from 'src/components/ContactCard';
-import {ContactDto} from 'src/types/dto/ContactDto';
+import { memo, useEffect } from 'react'
+import { Col, Row } from 'react-bootstrap'
+import { ContactCard } from 'src/components/ContactCard'
+import { fetchFavoriteContacts } from 'src/redux/favotire'
+import { useAppDispatch, useAppSelector } from 'src/redux/hooks'
 
-export const FavoritListPage = memo<CommonPageProps>(({
-  favoriteContactsState,
-  contactsState
-}) => {
-  const [contacts, setContacts] = useState<ContactDto[]>([])
-  useEffect(() => {
-    setContacts(() => contactsState[0].filter(({id}) => favoriteContactsState[0].includes(id)));
-  }, [contactsState, favoriteContactsState])
-  return (
-    <Row xxl={4} className="g-4">
-      {contacts.map((contact) => (
-        <Col key={contact.id}>
-          <ContactCard contact={contact} withLink />
-        </Col>
-      ))}
-    </Row>
-  );
+export const FavoritListPage = memo(() => {
+	const dispatch = useAppDispatch()
+
+	const { favoriteContactList } = useAppSelector(
+		state => state.favoriteContacts
+	)
+
+	useEffect(() => {
+		if (favoriteContactList.length === 0) {
+			dispatch(fetchFavoriteContacts())
+		}
+	}, [])
+	return (
+		<Row xxl={4} className='g-4'>
+			{favoriteContactList.map(contact => (
+				<Col key={contact.id}>
+					<ContactCard contact={contact} withLink />
+				</Col>
+			))}
+		</Row>
+	)
 })

8d8a066d8caa97554ebc4e6279242a31b49499df
sentares <aydararzybekov2002@mail.ru>
2025-07-16 14:25:37 +0600

    feat: update store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,8 @@
-import { applyMiddleware, combineReducers, createStore } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers } from 'redux'
 import { persistReducer, persistStore } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
-import thunkMiddleware from 'redux-thunk'
-import { contactsReducer } from './contacts'
+import { contactsApiSlice, contactsSlice } from './contacts'
 import { favoriteContactsReducer } from './favotire'
 import { groupsReducer } from './groups'
 import { logActionMiddleware } from './logActionMiddleware'
@@ -10,16 +10,24 @@ import { logActionMiddleware } from './logActionMiddleware'
 const rootReducer = persistReducer(
 	{ key: 'redux', storage: storage, throttle: 100000 },
 	combineReducers({
-		contacts: contactsReducer,
+		contacts: contactsSlice.reducer,
 		groups: groupsReducer,
 		favoriteContacts: favoriteContactsReducer,
+		[contactsApiSlice.reducerPath]: contactsApiSlice.reducer,
 	})
 )
 
-export const store = createStore(
-	rootReducer,
-	applyMiddleware(thunkMiddleware, logActionMiddleware)
-)
+export const store = configureStore({
+	reducer: rootReducer,
+	devTools: true,
+	middleware(getDefaultMiddleware) {
+		return getDefaultMiddleware({
+			serializableCheck: {
+				// ignoredActions: [FLUSH, REGISTER, REHYDRATE, PAUSE, PERSIST, PURGE],
+			},
+		}).concat([contactsApiSlice.middleware, logActionMiddleware])
+	},
+})
 
 export const persistor = persistStore(store)
 
